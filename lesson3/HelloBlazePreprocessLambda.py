e888d4977473ac1f985748635bf5af504125671c
Simona Mircheva <simona.mircheva@gmail.com>
2022-01-13 08:55:38 +0200

    Add Solution
    Create trigger for Lambda in S3 - invoke when a .zip file is uploaded

--- lesson3/HelloBlazePreprocessLambda.py
+++ lesson3/HelloBlazePreprocessLambda.py
@@ -1,13 +1,19 @@
 
 import json
 import zipfile
+import os
+import boto3
+from botocore.exceptions import ClientError
+
+BUCKET_NAME = 'udacity-ml-workflow'
+PREFIX = 'lambda'
 
 # Function below unzips the archive to the local directory. 
 
 def unzip_data(input_data_path):
     with zipfile.ZipFile(input_data_path, 'r') as input_data_zip:
-        input_data_zip.extractall('.')
-        return input_data_zip.namelist()[0]
+        input_data_zip.extractall('/tmp/')
+        return '/tmp/' + input_data_zip.namelist()[0]
 
 # Input data is a file with a single JSON object per line with the following format: 
 # {
@@ -46,6 +52,7 @@ def label_data(input_data):
           
     return labeled_data
 
+
 # Labeled data is a list of sentences, starting with the label defined in label_data. 
 
 def split_sentences(labeled_data):
@@ -58,7 +65,18 @@ def split_sentences(labeled_data):
                 new_split_sentences.append(" ".join([label, s]))
     return new_split_sentences
 
-def write_data(data, train_path, test_path, proportion):
+def upload_data(file_name):
+    object_name = os.path.join(PREFIX, os.path.basename(file_name))
+    s3_client = boto3.client('s3')
+    try:
+        response = s3_client.upload_file(file_name, BUCKET_NAME, object_name)
+    except ClientError as e:
+        logging.error(e)
+        return False
+
+def write_data(data, b_name, proportion):
+    train_path = '/tmp/' + b_name + '_train'
+    test_path = '/tmp/' +  b_name + '_test'
     border_index = int(proportion * len(data))
     train_f = open(train_path, 'w')
     test_f = open(test_path, 'w')
@@ -69,9 +87,23 @@ def write_data(data, train_path, test_path, proportion):
         else:
             test_f.write(d + '\n')
         index += 1
+    train_f.close()
+    test_f.close()
+    upload_data(train_path)
+    upload_data(test_path)
 
-if __name__ == "__main__":
-    unzipped_path = unzip_data('/opt/ml/processing/input/reviews_Musical_Instruments_5.json.zip')
+def download_data(s3_input_uri):
+    s3 = boto3.client('s3')
+    input_bucket = s3_input_uri.split('/')[0]
+    input_object = '/'.join(s3_input_uri.split('/')[1:])
+    file_name = '/tmp/' + os.path.basename(input_object)
+    s3.download_file(input_bucket, input_object, file_name)
+    return file_name
+        
+def preprocess(s3_input_uri):
+    f_name = download_data(s3_input_uri)
+    unzipped_path = unzip_data(f_name)
     labeled_data = label_data(unzipped_path)
     new_split_sentence_data = split_sentences(labeled_data)
-    write_data(new_split_sentence_data, '/opt/ml/processing/output/train/hello_blaze_train_scikit', '/opt/ml/processing/output/test/hello_blaze_test_scikit', .9)
+    write_data(new_split_sentence_data, os.path.basename(s3_input_uri), .9)
+
